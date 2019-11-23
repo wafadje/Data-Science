5ed5240be58004a90327c9042f711cc720717410
Djerad <wafa.djerad@live.fr>
2019-11-23 16:36:43 +0100

    Create main.py

---
+++ code/Machine Learning/03-Regression/Challenges/03-LR/src/main.py
@@ -0,0 +1,16 @@
+"""
+Remember how we implemented k-NN algorithm from scratch a few days ago?
+
+In this challenge, the goal is quite similar: implement the Linear Regression model from scratch.
+
+And when we say from scratch, this time it is really from scratch wink
+
+OK.., two small hints:
+
+    You should again, implement a class with the proper methods init, fit, predict
+    You can recode the gradient descent from scratch also... but maybe you will prefer to use the function linalg.lstsq from scipy library smirk
+    Separately, compute a function computing the sum of residual squares for your predictions
+
+Once you are done, test your new class on diabetes dataset from scikit-learn library. Compare your results with the LinearRegression from scikit-learn.
+
+Good luck padawan 🧙‍"""
