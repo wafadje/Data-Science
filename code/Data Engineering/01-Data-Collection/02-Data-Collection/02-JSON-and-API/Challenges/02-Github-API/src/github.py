546468ea7bfc83491093d321430163495e7aa41a
Djerad <wafa.djerad@live.fr>
2019-11-23 16:01:32 +0100

    Create github.py

---
+++ code/Data Engineering/01-Data-Collection/02-Data-Collection/02-JSON-and-API/Challenges/02-Github-API/src/github.py
@@ -0,0 +1,15 @@
+"""
+The Github API url is https://api.github.com.
+
+    Write a function get_description(repo) to retrieve the description of any given repository.
+    Write a function save_description(description, repo) storing a given description in a repo.json file located in the output folder
+    Create a list of 10 public Github repositories (you can browse the Explore menu on github.com to find some inspiration). Generate a dictionnary mapping those repos to their number of followers. Save your work in a followers.json file.
+
+Your code must be written in src/github.py. If needed, your can find the Github API documentation here.
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
