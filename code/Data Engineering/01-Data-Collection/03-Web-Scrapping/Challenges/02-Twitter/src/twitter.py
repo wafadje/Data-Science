fcb32738a11bcc71cb08ec3feed93eb4692dd009
Djerad <wafa.djerad@live.fr>
2019-11-23 16:05:00 +0100

    Create twitter.py

---
+++ code/Data Engineering/01-Data-Collection/03-Web-Scrapping/Challenges/02-Twitter/src/twitter.py
@@ -0,0 +1,17 @@
+"""
+Guidelines
+
+Write a Python script to get the number of followers of a given Twitter account.
+
+Your program should prompt the user for an account name and deal with wrong answers.
+
+Your code must be written in src/twitter.py.
+Bonus
+
+Want something more tricky? Try to scrap a given number of tweets of a given Twitter account.
+
+Store your tweets in output/tweets.json
+"""
+
+def main():
+  pass
