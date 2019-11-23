63125a96c075104c4e7a8d76435af80b83935e13
Djerad <wafa.djerad@live.fr>
2019-11-23 16:03:47 +0100

    Create daily_mirror.py

---
+++ code/Data Engineering/01-Data-Collection/03-Web-Scrapping/Challenges/01-Daily-Mirror/src/daily_mirror.py
@@ -0,0 +1,20 @@
+""" 
+Use the BeautifulSoup and requests Python packages to print out a list of all the article titles on the Daily Mirror homepage.
+
+Your program must be written in src/daily_mirror.py.
+"""
+# TODO: imports
+
+ 
+def main():
+    """
+    Don't forget to write a docstring.
+    """
+    # TODO: Use requests and BeautifulSoup to create the `soup` from the appropriate url.
+
+
+    # TODO: identify the news titles in the soup and print them out properly.
+ 
+
+if __name__ == "__main__":
+    main()
