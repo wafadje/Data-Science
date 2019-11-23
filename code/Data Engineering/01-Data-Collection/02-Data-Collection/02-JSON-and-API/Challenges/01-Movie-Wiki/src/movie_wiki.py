6d7b14abe00e23f3617f01ca1b02a78b47c02e22
Djerad <wafa.djerad@live.fr>
2019-11-23 16:00:42 +0100

    Create movie_wiki.py

---
+++ code/Data Engineering/01-Data-Collection/02-Data-Collection/02-JSON-and-API/Challenges/01-Movie-Wiki/src/movie_wiki.py
@@ -0,0 +1,15 @@
+# TODO: load `wiki_movie_plots.json` from `input` folder
+# Be careful : the file size is big so don't display it totally.
+
+
+# TODO: write a function to select only British movies
+
+
+# TODO: Save your python object in a new JSON file located in the `output`folder. 
+
+
+def main():
+        pass
+
+if __name__ == "__main__":
+    main()
