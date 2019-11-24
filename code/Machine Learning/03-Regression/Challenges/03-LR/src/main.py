d5779352c8d56ad47599b7153a261c38d817f560
Djerad <wafa.djerad@live.fr>
2019-11-24 10:31:16 +0100

    Update main.py

--- code/Machine Learning/03-Regression/Challenges/03-LR/src/main.py
+++ code/Machine Learning/03-Regression/Challenges/03-LR/src/main.py
@@ -14,3 +14,57 @@ OK.., two small hints:
 Once you are done, test your new class on diabetes dataset from scikit-learn library. Compare your results with the LinearRegression from scikit-learn.
 
 Good luck padawan 🧙‍"""
+
+import lib3_seaborn
+import matplotlib.pyplot as plt
+
+def get_gradient_at_b(x, y, b, m):
+  N = len(x)
+  diff = 0
+  for i in range(N):
+    x_val = x[i]
+    y_val = y[i]
+    diff += (y_val - ((m * x_val) + b))
+  b_gradient = -(2/N) * diff  
+  return b_gradient
+
+def get_gradient_at_m(x, y, b, m):
+  N = len(x)
+  diff = 0
+  for i in range(N):
+      x_val = x[i]
+      y_val = y[i]
+      diff += x_val * (y_val - ((m * x_val) + b))
+  m_gradient = -(2/N) * diff  
+  return m_gradient
+
+
+def step_gradient(b_current, m_current, x, y,learning_rate):
+    b_gradient = get_gradient_at_b(x, y, b_current, m_current)
+    m_gradient = get_gradient_at_m(x, y, b_current, m_current)
+    b = b_current - (learning_rate * b_gradient)
+    m = m_current - (learning_rate * m_gradient)
+    return [b, m]
+  
+
+def gradient_descent(x,y,learning_rate,num_iterations ):
+  b =0
+  m =0
+  for i in range(0, num_iterations):
+    [b,m] = step_gradient(b,m,x,y,learning_rate)
+  return [b,m]
+  
+months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+revenue = [52, 74, 79, 95, 115, 110, 129, 126, 147, 146, 156, 184]
+
+
+b, m = gradient_descent(months, revenue, 0.01, 1000)
+
+
+
+y = [m*x + b for x in months]
+
+plt.plot(months, revenue, "o")
+plt.plot(months, y)
+
+plt.show()
