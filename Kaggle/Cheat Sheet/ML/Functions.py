a19367364936b2c0ad2ace05296aeb2fab94b797
Djerad <wafa.djerad@live.fr>
2020-11-17 16:11:36 +0100

    Update Functions.py

--- Kaggle/Cheat Sheet/ML/Functions.py
+++ Kaggle/Cheat Sheet/ML/Functions.py
@@ -1,3 +1,14 @@
+import numpy as np 
+import pandas as pd 
+from matplotlib import pyplot as plt
+import seaborn as sns
+from IPython.display import display
+
+%matplotlib inline
+
+
+
+
 def missing_values(df):
     missing=pd.DataFrame(df.isnull().sum()/len(data))*100
     missing.columns = ['missing_values(%)']
